1774174fbeb245b7e92d4f7d73be5081e704c071
Avishek <avishekprasad12345@gmail.com>
2025-08-16 00:33:37 +0530

    today's updates: <ResumeCard, NavBar, ScoreCircle etc>

---
+++ app/Components/NavBar.tsx
@@ -0,0 +1,15 @@
+import {Link} from "react-router";
+import type {JSX} from "react";
+
+const Navbar = () => {
+    return (
+        <nav className="navbar">
+            <Link to="/">
+                <p className="text-2xl font-bold text-gradient">RESUMIND</p>
+            </Link>
+            <Link to="/upload" className="primary-button w-fit">Upload Resume</Link>
+        </nav>
+    )
+}
+
+export default Navbar
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,4 +1,7 @@
 import type { Route } from "./+types/home";
+import NavBar from "~/Components/NavBar";
+import {resumes} from "../../constants";
+import ResumeCard from "../Components/ResumeCard";
 
 
 export function meta({}: Route.MetaArgs) {
@@ -10,13 +13,23 @@ export function meta({}: Route.MetaArgs) {
 
 export default function Home() {
   return <main className="bg-[url('/images/bg-main.svg')] bg-cover">
+      <NavBar/>
       <section className="main-section">
           <div className="page-heading">
             <h1>Track your Application & Resume Ratings</h1>
               <h2>Review your submission and check AI-powered feedback.</h2>
           </div>
+          {resumes.length > 0 && (
+              <div className="resume-section">
+                  {resumes.map((resume)=>(
+                      <ResumeCard key={resume.id} resume={resume}/>
+                  ))}
+
+              </div>
+          )}
 
       </section>
+
 
   </main>
 }
